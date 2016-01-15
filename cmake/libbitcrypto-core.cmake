160b81e6fc11e0954eb19fe5008a3e4e2709d7aa
Victor Bucataru <ping@devktor.com>
2016-01-15 15:29:41 +0200

    added detail headers

--- cmake/libbitcrypto-core.cmake
+++ cmake/libbitcrypto-core.cmake
@@ -2,7 +2,7 @@ find_package(Boost COMPONENTS system REQUIRED)
 
 #add_definitions(-D__DEBUG__)
 
-include_directories(${CMAKE_BINARY_DIR}/include/bitcrypto ${CMAKE_SOURCE_DIR}/src)
+include_directories(${CMAKE_SOURCE_DIR}/src ${CMAKE_BINARY_DIR}/include/bitcrypto)
 
 
 file(GLOB SOURCES 
@@ -17,6 +17,7 @@ file(GLOB HEADERS RELATIVE ${CMAKE_SOURCE_DIR}/src
     "src/*.hpp"
     "src/*.ipp"
     "src/external/*.h"
+    "src/detail/*.hpp"
 )
 
 
@@ -26,11 +27,11 @@ file(GLOB HEADERS RELATIVE ${CMAKE_SOURCE_DIR}/src
 add_library(bitcrypto-core STATIC ${SOURCES})
 add_dependencies(bitcrypto-core libsecp256k1)
 
-add_custom_command(
-    TARGET bitcrypto-core
-    PRE_BUILD
-    COMMAND ${CMAKE_COMMAND} ARGS -E make_directory ${CMAKE_BINARY_DIR}/include/bitcrypto/external
-)
+#add_custom_command(
+#    TARGET bitcrypto-core
+#    PRE_BUILD
+#    COMMAND ${CMAKE_COMMAND} ARGS -E make_directory ${CMAKE_BINARY_DIR}/include/bitcrypto/external
+#)
 
 foreach(HEADER ${HEADERS})
     add_custom_command(
