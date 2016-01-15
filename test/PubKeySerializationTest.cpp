4193daf276c4e0a35af2504b8bcce2d0093ef4b6
Victor Bucataru <ping@devktor.com>
2016-01-15 06:12:26 +0200

    unit tests for compressed/uncompressed points serialization/deserialization

--- test/PubKeySerializationTest.cpp
+++ test/PubKeySerializationTest.cpp
@@ -9,23 +9,41 @@ BOOST_AUTO_TEST_SUITE(PubKeySerializerTest)
 
 
 
-BOOST_AUTO_TEST_CASE(serializationTest)
+BOOST_AUTO_TEST_CASE(fromCompressedPoint)
 {
-    Base16Encoder encoder;
     Base16PubKeySerializer serializer;
     PubKey key = serializer.unserialize("0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
 
     BOOST_REQUIRE_EQUAL(serializer.serialize(key), "0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
 
+    BOOST_REQUIRE_EQUAL(serializer.serialize(key, false), "0409ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc7327318c3a6ec6acd33c36328b8fb4349b31671bcd3a192316ea4f6236ee1ae4a7d8c9");
+
 }
 
 
-BOOST_AUTO_TEST_CASE(deserializationError)
+BOOST_AUTO_TEST_CASE(fromUncompressedPoint)
+{
+    Base16PubKeySerializer serializer;
+    PubKey key = serializer.unserialize("0409ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc7327318c3a6ec6acd33c36328b8fb4349b31671bcd3a192316ea4f6236ee1ae4a7d8c9");
+
+    BOOST_REQUIRE_EQUAL(serializer.serialize(key), "0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
+
+    BOOST_REQUIRE_EQUAL(serializer.serialize(key, false), "0409ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc7327318c3a6ec6acd33c36328b8fb4349b31671bcd3a192316ea4f6236ee1ae4a7d8c9");
+}
+
+
+BOOST_AUTO_TEST_CASE(invalidCompressedPoint)
 {
     Base16PubKeySerializer serializer;
     BOOST_CHECK_THROW(serializer.unserialize("0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc73"), std::runtime_error);
 }
 
+
+BOOST_AUTO_TEST_CASE(invalidUncompressedPoint)
+{
+    Base16PubKeySerializer serializer;
+    BOOST_CHECK_THROW(serializer.unserialize("0409ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc7327318c3a6ec6acd33c36328b8fb4349b31671bcd3a192316ea4f6236ee1ae4a7"), std::runtime_error);
+}
 
 
 
