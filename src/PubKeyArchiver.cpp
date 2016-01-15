e2f30e621fbd563b3c2236c07742fa238f426b26
Victor Bucataru <ping@devktor.com>
2016-01-15 03:04:27 +0200

    allow raw data in PubKeyArchiver

--- src/PubKeyArchiver.cpp
+++ src/PubKeyArchiver.cpp
@@ -4,15 +4,13 @@
 namespace BitCrypto{
 
 
-PubKeyArchiver::PubKeyArchiver()
-{
-    Secp256k1ContextFactory factory;
-    _context = factory.create();
-}
+PubKeyArchiver::PubKeyArchiver() :
+    _context(createContext())
+{}
 
 
 PubKeyArchiver::PubKeyArchiver(const Secp256k1ContextPtr &context) : 
-    _context(context)
+    _context(context?context:createContext())
 {}
 
 
@@ -22,6 +20,30 @@ CompressedPoint PubKeyArchiver::compress(const PubKey &key)
     size_t pointSize = point.size();
 
     if(!secp256k1_ec_pubkey_serialize(_context.get(), point.data(), &pointSize, &key, SECP256K1_EC_COMPRESSED))
+    {
+        throw std::runtime_error("failed to compress pubkey");
+    }
+
+    if(pointSize!=point.size())
+    {
+        throw std::runtime_error("invalid compressed point");
+    }
+
+    return point;
+}
+
+
+CompressedPoint PubKeyArchiver::compress(const Data &key)
+{
+    if(key.size()!=64)
+    {
+        throw std::runtime_error("invalid key");
+    }
+
+    CompressedPoint point;
+    size_t pointSize = point.size();
+
+    if(!secp256k1_ec_pubkey_serialize(_context.get(), point.data(), &pointSize, reinterpret_cast<const secp256k1_pubkey*>(&*key.begin()), SECP256K1_EC_COMPRESSED))
     {
         throw std::runtime_error("failed to compress pubkey");
     }
@@ -49,12 +71,24 @@ PubKey PubKeyArchiver::decompress(const CompressedPoint &point)
 
 PubKey PubKeyArchiver::decompress(const Data &data)
 {
+    if(data.size()!=33)
+    {
+        throw std::runtime_error("invalid compressed point");
+    }
+
     PubKey key(_context);
     if(!secp256k1_ec_pubkey_parse(_context.get(), &key, data.data(), data.size()))
     {
         throw std::runtime_error("failed to decompress key");
     }
     return key;
+}
+
+
+Secp256k1ContextPtr PubKeyArchiver::createContext()
+{
+    Secp256k1ContextFactory factory;
+    return factory.create();
 }
 
 }
--- src/PubKeyArchiver.hpp
+++ src/PubKeyArchiver.hpp
@@ -13,14 +13,19 @@ namespace BitCrypto{
 
 class PubKeyArchiver 
 {
+
     public:
+
         PubKeyArchiver();
         PubKeyArchiver(const Secp256k1ContextPtr &);
 
         CompressedPoint compress(const PubKey &);
+        CompressedPoint compress(const Data &);
         PubKey decompress(const CompressedPoint &);
         PubKey decompress(const Data &);
 
+    private:
+        Secp256k1ContextPtr createContext();
 
     private:
         Secp256k1ContextPtr _context;
