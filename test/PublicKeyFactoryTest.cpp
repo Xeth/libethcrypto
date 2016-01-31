a11834e3422d56b4e793ce32ac865e2a5ec08134
Victor Bucataru <ping@devktor.com>
2016-01-31 16:35:25 +0200

    public key serializer facade

--- test/PublicKeyFactoryTest.cpp
+++ test/PublicKeyFactoryTest.cpp
@@ -1,8 +1,5 @@
 #include <boost/test/unit_test.hpp>
 
-#include "ethkey/serialization/Base16PrivateKeySerializer.hpp"
-#include "ethkey/serialization/Base16PublicKeySerializer.hpp"
-
 #include "ethkey/PublicKeyFactory.hpp"
 #include "ethkey/Literal.hpp"
 
@@ -23,23 +20,19 @@ BOOST_AUTO_TEST_CASE(fromPrivateKey)
 
 BOOST_AUTO_TEST_CASE(fromCompressedPoint)
 {
-    Base16PublicKeySerializer serializer;
     PublicKeyFactory factory;
-    CompressedPoint point;
-    serializer.unserialize("0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731", point);
+    CompressedPoint point = Literal<CompressedPoint>("0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
     PublicKey pub = factory.createFromPoint(point);
-    BOOST_REQUIRE_EQUAL(serializer.serialize(pub), "0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
+    BOOST_REQUIRE_EQUAL(Literal(pub), "0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
 
 }
 
 BOOST_AUTO_TEST_CASE(fromUncompressedPoint)
 {
-    Base16PublicKeySerializer serializer;
     PublicKeyFactory factory;
-    UncompressedPoint point;
-    serializer.unserialize("0409ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc7327318c3a6ec6acd33c36328b8fb4349b31671bcd3a192316ea4f6236ee1ae4a7d8c9", point);
+    UncompressedPoint point = Literal<UncompressedPoint>("0409ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc7327318c3a6ec6acd33c36328b8fb4349b31671bcd3a192316ea4f6236ee1ae4a7d8c9");
     PublicKey pub = factory.createFromPoint(point);
-    BOOST_REQUIRE_EQUAL(serializer.serialize(pub), "0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
+    BOOST_REQUIRE_EQUAL(Literal(pub), "0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
 }
 
 
--- test/PublicKeySerializationTest.cpp
+++ test/PublicKeySerializationTest.cpp
@@ -1,6 +1,6 @@
 #include <boost/test/unit_test.hpp>
 
-#include "ethkey/serialization/Base16PublicKeySerializer.hpp"
+#include "ethkey/serialization/PublicKeySerializer.hpp"
 
 
 using namespace Ethereum;
@@ -11,7 +11,7 @@ BOOST_AUTO_TEST_SUITE(PublicKeySerializerTest)
 
 BOOST_AUTO_TEST_CASE(fromCompressedPoint)
 {
-    Base16PublicKeySerializer serializer;
+    PublicKeySerializer serializer;
     PublicKey key = serializer.unserialize("0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
 
     BOOST_REQUIRE_EQUAL(serializer.serialize(key), "0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
@@ -23,7 +23,7 @@ BOOST_AUTO_TEST_CASE(fromCompressedPoint)
 
 BOOST_AUTO_TEST_CASE(fromUncompressedPoint)
 {
-    Base16PublicKeySerializer serializer;
+    PublicKeySerializer serializer;
     PublicKey key = serializer.unserialize("0409ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc7327318c3a6ec6acd33c36328b8fb4349b31671bcd3a192316ea4f6236ee1ae4a7d8c9");
 
     BOOST_REQUIRE_EQUAL(serializer.serialize(key), "0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc732731");
@@ -34,14 +34,14 @@ BOOST_AUTO_TEST_CASE(fromUncompressedPoint)
 
 BOOST_AUTO_TEST_CASE(invalidCompressedPoint)
 {
-    Base16PublicKeySerializer serializer;
+    PublicKeySerializer serializer;
     BOOST_CHECK_THROW(serializer.unserialize("0309ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc73"), std::runtime_error);
 }
 
 
 BOOST_AUTO_TEST_CASE(invalidUncompressedPoint)
 {
-    Base16PublicKeySerializer serializer;
+    PublicKeySerializer serializer;
     BOOST_CHECK_THROW(serializer.unserialize("0409ba8621aefd3b6ba4ca6d11a4746e8df8d35d9b51b383338f627ba7fc7327318c3a6ec6acd33c36328b8fb4349b31671bcd3a192316ea4f6236ee1ae4a7"), std::runtime_error);
 }
 
