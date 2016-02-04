1d283cd565585189aa2e2e9088a5520c53c89df4
Victor Bucataru <ping@devktor.com>
2016-02-05 00:12:52 +0200

    generic decoding

--- src/encoding/HexEncoder.hpp
+++ src/encoding/HexEncoder.hpp
@@ -8,11 +8,13 @@ namespace Ethereum{
 typedef Base16Encoder HexEncoder;
 
 
-inline std::string EncodeHex(const Data &data)
+template<class Data>
+std::string EncodeHex(const Data &data)
 {
     HexEncoder encoder;
-    return encoder.encode(data);
+    return encoder.encode(data.begin(), data.end());
 }
+
 
 template<class Iterator>
 std::string EncodeHex(Iterator begin, Iterator end)
@@ -21,6 +23,12 @@ std::string EncodeHex(Iterator begin, Iterator end)
     return encoder.encode(begin, end);
 }
 
+template<class Data>
+Data DecodeHex(const std::string &hex)
+{
+    HexEncoder encoder;
+    return encoder.decode<Data>(hex.begin(), hex.end());
+}
 
 inline Data DecodeHex(const std::string &hex)
 {
