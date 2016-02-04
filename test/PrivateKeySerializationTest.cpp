99c32c8f81045680a3f2d0adbb64a359e9f84480
Victor Bucataru <ping@devktor.com>
2016-02-04 03:23:33 +0200

    private key serialization test

---
+++ test/PrivateKeySerializationTest.cpp
@@ -0,0 +1,33 @@
+#include <boost/test/unit_test.hpp>
+
+#include "ethkey/cipher/AesCipher.hpp"
+#include "ethkey/cipher/Scrypt.hpp"
+#include "ethkey/serialization/PrivateKeySerializer.hpp"
+#include "ethkey/KeyGenerator.hpp"
+
+using namespace Ethereum;
+
+BOOST_AUTO_TEST_SUITE(PrivateKeySerializerTest)
+
+typedef AesCipher<Scrypt> Cipher;
+
+BOOST_AUTO_TEST_CASE(serialization)
+{
+    KeyGenerator generator;
+    PrivateKeySerializer<Cipher> serializer;
+
+    SecuredKeyPair<Cipher> key = generator.generate(Cipher(MakeRandomIV(), MakeRandomScryptParams()), "asdasd123");
+
+    std::string serialized = serializer.serialize(key.getPrivateKey());
+
+    SecuredPrivateKey<Cipher> key2 = serializer.unserialize(serialized);
+
+    BOOST_REQUIRE(key.getPrivateKey().unlock("asdasd123") == key2.unlock("asdasd123"));
+    BOOST_REQUIRE(key.getPrivateKey().getData().data == key2.getData().data);
+    BOOST_REQUIRE(key.getPrivateKey().getData().mac == key2.getData().mac);
+}
+
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
