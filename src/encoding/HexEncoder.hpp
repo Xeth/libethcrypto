bb0f85f4284776fd359a9023d0f21f00078956ad
Victor Bucataru <ping@devktor.com>
2016-02-04 02:40:10 +0200

    encode/decode hex method

--- src/encoding/HexEncoder.hpp
+++ src/encoding/HexEncoder.hpp
@@ -8,4 +8,27 @@ namespace Ethereum{
 typedef Base16Encoder HexEncoder;
 
 
+inline std::string EncodeHex(const Data &data)
+{
+    HexEncoder encoder;
+    return encoder.encode(data);
+}
+
+template<class Iterator>
+std::string EncodeHex(Iterator begin, Iterator end)
+{
+    HexEncoder encoder;
+    return encoder.encode(begin, end);
+}
+
+
+inline Data DecodeHex(const std::string &hex)
+{
+    HexEncoder encoder;
+    return encoder.decode(hex);
+}
+
+
+
+
 }
