fcff97a691ad93db8b5d9f05b2f13e0e234b7bc7
Victor Bucataru <ping@devktor.com>
2016-02-08 02:16:16 +0200

    cmake modules update

--- cmake/libethkey-core.cmake
+++ cmake/libethkey-core.cmake
@@ -19,35 +19,6 @@ file(GLOB SOURCES
     "src/hash/*.cpp"
 )
 
-include (${CMAKE_CURRENT_SOURCE_DIR}/cmake/modules/CopyHeaders.cmake)
-CopyHeaders()
-
-
-#file(GLOB HEADERS RELATIVE ${CMAKE_CURRENT_SOURCE_DIR}/src
-#    "src/*.hpp"
-#    "src/*.ipp"
-#    "src/external/*.h"
-#    "src/detail/*.hpp"
-#    "src/detail/*.ipp"
-#    "src/serialization/*.hpp"
-#    "src/serialization/*.ipp"
-#    "src/serialization/detail/*.hpp"
-#    "src/serialization/detail/*.ipp"
-#    "src/encoding/*.hpp"
-#    "src/encoding/*.ipp"
-#    "src/encoding/detail/*.hpp"
-#    "src/encoding/detail/*.ipp"
-#    "src/cipher/*.hpp"
-#    "src/cipher/*.ipp"
-#    "src/hash/*.hpp"
-#    "src/hash/*.ipp"
-#)
-
-#message(${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp)
-
-#add_library(ethkey-dummy STATIC ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp)
-
-
 
 add_library(ethkey-core STATIC ${SOURCES})
 add_dependencies(ethkey-core libsecp256k1 )
