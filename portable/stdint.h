376537a47b136831665749b958534d8e311b0dea
Victor Bucataru <ping@devktor.com>
2016-02-29 20:12:25 +0200

    int128 type

--- portable/stdint.h
+++ portable/stdint.h
@@ -7,4 +7,9 @@ typedef unsigned __int32 uint32_t;
 typedef __int64 int64_t;
 typedef unsigned __int64 uint64_t;
 
+#if HAVE___INT128
+typedef __int128 int128_t;
+typedef unsigned __int128 uint128_t;
+#endif
+
 #endif
