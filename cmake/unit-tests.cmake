10ce0aab875782ba7a92c8bc3532d6e2120c2c07
Victor Bucataru <ping@devktor.com>
2016-03-17 18:21:37 +0200

    linking pthread

--- cmake/unit-tests.cmake
+++ cmake/unit-tests.cmake
@@ -6,14 +6,24 @@ file(GLOB TEST_SOURCES
 )
 
 
-
+set(CMAKE_THREAD_PREFER_PTHREAD ON)
+find_package(Threads REQUIRED)
 
 include_directories(${Boost_INCLUDE_DIRS} ${CRYPTOPP_INCLUDE_DIR} ${JSONCPP_INCLUDE_DIR} ${CMAKE_CURRENT_BINARY_DIR}/include)
 
 add_executable(ethkey-tests ${TEST_SOURCES})
 add_dependencies(ethkey-tests ethkey)
 
-target_link_libraries(ethkey-tests ethkey ${Boost_FILESYSTEM_LIBRARY} ${Boost_SYSTEM_LIBRARY} ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY} ${Boost_RANDOM_LIBRARY} ${CRYPTOPP_LIBRARY}  ${JSONCPP_LIBRARY})
+target_link_libraries(ethkey-tests 
+    ethkey
+    ${Boost_FILESYSTEM_LIBRARY}
+    ${Boost_SYSTEM_LIBRARY}
+    ${Boost_UNIT_TEST_FRAMEWORK_LIBRARY}
+    ${Boost_RANDOM_LIBRARY}
+    ${CRYPTOPP_LIBRARY}
+    ${JSONCPP_LIBRARY}
+    ${CMAKE_THREAD_LIBS_INIT}
+)
 
 if(GMP_LIBRARIES)
     target_link_libraries(ethkey-tests ${GMP_LIBRARIES})
