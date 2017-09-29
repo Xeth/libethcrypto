03501780f193cb4489162901f5b67a88fe3c5759
Victor Bucataru <ping@devktor.com>
2017-09-29 22:44:05 +0300

    fixed cryptopp build

--- cmake/cryptopp.cmake
+++ cmake/cryptopp.cmake
@@ -1,3 +1,20 @@
-add_subdirectory(cryptopp ${PROJECT_BINARY_DIR}/lib.obj)
 set(CRYPTOPP_INCLUDE_DIR ${PROJECT_SOURCE_DIR})
 set(CRYPTOPP_LIBRARY cryptopp-static)
+
+include(ExternalProject)
+
+MESSAGE("------------------------------------------------")
+MESSAGE("${CMAKE_CURRENT_BINARY_DIR}/lib.obj")
+message("make -C ${CMAKE_CURRENT_SOURCE_DIR}/cryptopp")
+
+ExternalProject_Add(
+  cryptopp
+  SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/cryptopp
+  CONFIGURE_COMMAND ""
+  INSTALL_COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/cryptopp/libcryptopp.a ${CMAKE_CURRENT_BINARY_DIR}/lib.obj
+  BUILD_COMMAND make -C ${CMAKE_CURRENT_SOURCE_DIR}/cryptopp
+#  BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/lib.obj
+  BUILD_IN_SOURCE 1
+  LOG_CONFIGURE 1
+  LOG_BUILD 1
+)
